ba58e9bc0f99fb3525270a711646401ddaa77200
Rafael Pantoja <rafaelbp92@gmail.com>
2024-03-13 20:55:55 +0000

    Initial location handling

--- components/Places/LocationPicker.tsx
+++ components/Places/LocationPicker.tsx
@@ -1,38 +1,74 @@
-import { StyleSheet, View } from "react-native"
-import OutlinedButton from "../ui/OutlinedButton"
+import { Alert, StyleSheet, View } from "react-native";
+import {
+  PermissionStatus,
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+} from "expo-location";
+import OutlinedButton from "../ui/OutlinedButton";
 import { Colors } from "../../constants/Colors";
 
 const LocationPicker = () => {
-    function getLocationHandler() { }
+  const [locationPermission, requestPermission] = useForegroundPermissions();
 
-    function pickOnMapHandler() { }
+  async function verifyPermissions() {
+    if (locationPermission?.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
 
-    return (
-        <View>
-            <View style={styles.mapPreview}></View>
-            <View style={styles.actions}>
-                <OutlinedButton icon="location" onPress={getLocationHandler}>Locate User</OutlinedButton>
-                <OutlinedButton icon="map" onPress={pickOnMapHandler}>Pick on Map</OutlinedButton>
-            </View>
-        </View>
-    )
-}
+      return permissionResponse.granted;
+    }
+
+    if (locationPermission?.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Required permissions",
+        "The app needs permission to the camera"
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+    const location = await getCurrentPositionAsync();
+  }
+
+  function pickOnMapHandler() {}
+
+  return (
+    <View>
+      <View style={styles.mapPreview}></View>
+      <View style={styles.actions}>
+        <OutlinedButton icon="location" onPress={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon="map" onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutlinedButton>
+      </View>
+    </View>
+  );
+};
 
 export default LocationPicker;
 
 const styles = StyleSheet.create({
-    mapPreview: {
-        width: '100%',
-        height: 200,
-        marginVertical: 8,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: Colors.primary100,
-        borderRadius: 4
-    },
-    actions: {
-        flexDirection: "row",
-        justifyContent: "space-around",
-        alignItems: "center"
-    }
+  mapPreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
 });
