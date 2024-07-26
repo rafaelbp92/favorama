e0110254f7be4a3a6d8e9c84c9ed2da66bef9951
Rafael Pantoja <rafaelbp92@gmail.com>
2024-07-26 23:26:17 +0100

    Removing key from repo

--- utils/location.ts
+++ utils/location.ts
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyAKy2AxZx0Tcl81w_VMaN20Rl6KbQrFMMs";
+const GOOGLE_API_KEY = "GOOGLE_API_KEY";
 
 export function getMapPreview(lat: number, lng: number) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat}, ${lng}&zoom=14&size=400x200&maptype=roadmap&markers=red:blue%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
